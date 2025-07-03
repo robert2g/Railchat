f27658e056d06a293593a0d0ed7166cb1b73ead5
robert2g <jose.gglassman@proton.me>
2025-07-02 23:04:13 -0500

    Initial Commit - Basic set up of user controller.

---
+++ API/Program.cs
@@ -0,0 +1,19 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// HTTP request pipeline.
+app.MapControllers();
+
+app.Run();
